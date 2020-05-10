229e761b22e10a0897e00220c5464c0ded4cff04
PINo12 <mwww-go@yandex.ru>
2020-05-10 16:03:14 +0400

    аннотации2

--- src/test/java/Setting/testSetting.java
+++ src/test/java/Setting/testSetting.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+//import io.github.bonigarcia.wdm.ChromeDriverManager;
 
 public class testSetting {
 
@@ -14,14 +15,15 @@ public class testSetting {
 
 
     public static void start(){
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        WebDriverRunner.setWebDriver(driver);
+      //  System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        //WebDriver driver = new ChromeDriver();
+      //  driver.manage().window().maximize();
+      //  WebDriverRunner.setWebDriver(driver);
         Configuration.timeout = 6000;
+        Configuration.startMaximized= true;
     }
 
     public static void close(){
-        WebDriverRunner.closeWebDriver();
+       // WebDriverRunner.closeWebDriver();
     }
 }
